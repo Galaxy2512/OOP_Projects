79507b031c092d40447c9846ab371b38248b9576
Kristina <kseparovi@student.unizd.hr>
2023-10-17 22:34:47 +0200

    Commit

--- Basics/src/ObjectsAndMethods/Car.java
+++ Basics/src/ObjectsAndMethods/Car.java
@@ -2,21 +2,23 @@ package ObjectsAndMethods;
 
 public class Car {
 
-    String brand;
-    String model;
-    int year;
-    String type;
-    int hp;
+    public static String brand;
+    public static String model;
+    public static int year;
+    public static String type;
+    public static int hp;
 
-    public Car (String brand, String model, int year, String type, int hp){
+    public Car (String brand, String model, int year, String type, int hp) {
         this.brand = brand;
         this.hp = hp;
         this.model = model;
         this.year = year;
         this.type = type;
-
-
     }
+
+
+
+
     public static void drive(){
         System.out.println("Car drive");
 
@@ -28,12 +30,46 @@ public class Car {
 
     @Override
     public String toString() { //to string method to represent string values of object
-        return "Car{" +
-                "brand='" + brand + '\'' +
-                ", model='" + model + '\'' +
-                ", year=" + year +
-                ", type='" + type + '\'' +
-                ", hp=" + hp +
-                '}';
+        return "Car{" + "brand='" + brand + '\'' + ", model='" + model + '\'' + ", year=" + year + ", type='" + type + '\'' + ", hp=" + hp + '}';
     }
+
+//    public static String getBrand() {;
+//        return brand;
+//    }
+//
+//    public static void setBrand(String brand) {
+//        Car.brand = brand;
+//    }
+//
+//    public static String getModel() {
+//        return model;
+//    }
+//
+//    public static void setModel(String model) {
+//        Car.model = model;
+//    }
+//
+//    public static int getYear() {
+//        return year;
+//    }
+//
+//    public static void setYear(int year) {
+//        Car.year = year;
+//    }
+//
+//    public static String getType() {
+//        return type;
+//    }
+//
+//    public static void setType(String type) {
+//        Car.type = type;
+//    }
+//
+//    public static int getHp() {
+//        return hp;
+//    }
+//
+//    public static void setHp(int hp) {
+//        Car.hp = hp;
+//    }
 }
